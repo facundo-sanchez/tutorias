c79def0842a7821d41cc83253a0835d3e4f66d41
Facundo Sanchez <facuujos@gmail.com>
2023-03-31 19:17:40 -0300

    segundo commit

---
+++ 01/main.js
@@ -0,0 +1,31 @@
+"use strict";
+exports.__esModule = true;
+// let name: String = rls.question("Ingrese nombre");
+// let apellido: String = rls.question("Ingrese apellido");
+// SI name === Facundo -> console.log(Tu nombre es facundo y ya esta en uso) :  console.log(nombre)
+// '1' == 1
+// ==
+// Primero condicon... se cumple? la ejecuto sino se cumple ejecuta la segunda condicion
+// if (name === "Facundo") {
+//   console.log("Tu nombre es facundo y ya esta en uso");
+// } else if (apellido === "Sanchez") {
+//   console.log("Tu apellido es Sanchez y ya esta en uso");
+// } else {
+//   console.log("Error");
+// }
+// switch (name) {
+//   case "Facundo":
+//     console.log("Hola Facundo");
+//     break;
+//   case "Martin":
+//     console.log("Hola Martin");
+//     break;
+//   case "Kevin":
+//     console.log("Hola Kevin");
+//     break;
+//   default:
+//     console.log("El nombre no esta registrado.");
+//     break;
+// }
+var name = "Hola!";
+console.log(name);
